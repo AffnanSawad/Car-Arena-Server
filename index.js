985cde2d81dd3d9085075b6f5e66648e60f4870b
Affnan Sawad <affnansawad2002@gmail.com>
2024-05-06 14:16:15 +0600

    jwt no buji vai , abar porte hbe , module - 60

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 
+const jwt = require('jsonwebtoken');
+
+
+
+const cookieParser = require('cookie-parser');
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
@@ -10,8 +17,14 @@ const port = process.env.PORT || 5000 ;
 
 //MIDDLE WARE 
 
-app.use( cors())
+app.use( cors({
+
+  origin: ['http://localhost:5173'],
+  credentials:true
+}   ))
 app.use(express.json())
+
+app.use(cookieParser())
 
 
 
@@ -36,6 +49,43 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+//my middleware
+
+const logger = async(req,res,next) => {
+  console.log('called', req.host , req.originalUrl  )
+
+  next()
+}
+
+
+const verifyToken = async(req,res,next) => {
+  
+  const token = req.cookies?.token;
+
+  console.log('middle of toeken in middleware', token)
+
+  if(!token){
+    return res.status(401).send({message:'not authorized'})
+  }
+
+
+  jwt.verify(token,process.env.Access_token , (err,decoded)=>{
+    if(err){
+
+      console.log(err)
+      return res.status(401).send({message: 'unauthorized'})
+    }
+
+    console.log('value in the token',decoded)
+  req.user = decoded;
+    next()
+  } )
+
+  
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -47,8 +97,44 @@ async function run() {
    
     const orderscollection = client.db('car_arena').collection('orders');
 
+   
 
-    app.get('/services', async(req,res) => {
+  //auth / jwt related api
+
+  app.post('/jwt', logger,   async(req, res) => {
+       
+    const user = req.body 
+    console.log(user)
+
+    const token = jwt.sign(user , process.env.Access_token, {expiresIn: '1h'} )
+ 
+    
+    res
+
+    .cookie('token', token , {
+      httpOnly:true,
+      secure:false,
+      sameSite:'none'
+    })
+
+    .send({success: true});
+
+
+
+}
+)
+  
+
+
+
+
+
+
+
+
+
+    //service related api
+    app.get('/services', logger,async(req,res) => {
 
         const cursor = servicecollection.find()
    
@@ -79,7 +165,7 @@ async function run() {
     const options = {
      
         // Include only the `title` and `imdb` fields in the returned document
-        projection: {  title: 1,  price: 1 ,  service_id: 1  },
+        projection: {  title: 1,  price: 1 ,  service_id: 1 , img:1 },
       };
     
 
@@ -90,6 +176,47 @@ async function run() {
   
 
    }  )
+
+
+   // jesob oder dewa hoise segula onno page e dekano... nij user er jnno
+
+   app.get('/orders',logger, async(req,res) => {
+   
+  
+    //querry , jate kore user tar orders dekte pare... querry maddhme fielter kora hoi orders.
+
+    console.log(req.query.email);
+
+   // console.log('tokennnnnn',req.cookies.token)
+
+   console.log('user in the valid token ', req.user )
+
+
+   if(req.query.email !== req.user.email){
+
+    return res.status(403).send({message:'forbidden access'})
+   }
+
+    let query = {};
+
+    if(req.query?.email){
+
+      query = {email : req.query.email}
+    }
+
+
+
+
+
+    const cursor = orderscollection.find(query)
+
+    const result = await cursor.toArray();
+
+    res.send(result);
+
+
+   }  )
+
 
 
 
@@ -109,6 +236,61 @@ async function run() {
 
 }
 )
+
+
+app.patch('/orders/:id',async(req,res) => {
+
+
+  const id = req.params.id
+  const filter = { _id: new ObjectId(id)}
+
+
+
+
+
+ const updatedbooking = req.body;
+
+
+ const uporder = {
+     
+  $set: {
+      status : updatedbooking.status
+     
+  },
+
+
+};
+
+const result = await orderscollection.updateOne(filter,uporder)
+
+ console.log(result)
+
+
+}
+
+
+
+
+
+) 
+
+
+
+//database theke delete kora . ERPOR CLIENT SIDE E EVENT HANDLE E FETCH KORTE HBE.
+app.delete('/orders/:id', async(req,res) => {
+
+  const id = req.params.id
+
+  console.log('delted from database',id)
+
+  const query = { _id: new ObjectId(id)};
+  
+  const result = await orderscollection.deleteOne(query)
+   
+  res.send(result);
+
+
+ }  )
 
 
 
